a442562cd1fd945ec8b28fce936c7877ce9c5545
Nadia Kasim <nadiakasim@hotmail.com>
2025-02-13 12:39:19 -0500

    fixed file imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import './App.css'
+import "./App.scss";
 
 function App() {
-
   return (
     <>
       <h1>This is our Industry Project</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
