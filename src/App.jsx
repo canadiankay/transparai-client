8d51b99e9edb2ee6e75735ad178d394896f2fad7
Aaron Mahoney <aaronmahoney1993@outlook.com>
2025-02-13 20:47:56 -0700

    switched colours, fonts, added missing icons, and imported components to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.scss";
 import ResponseWindow from "./ResponseWindow/ResponseWindow";
 import ResourcesList from "./components/ResourcesList/ResourcesList.jsx";
 import Nav from "./components/Nav/Nav.jsx";
+import Hero from "./components/Hero/Hero.jsx";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <h1>This is our Industry Project</h1>
       <ResponseWindow />
       <Nav />
+      <Hero />
       <ResourcesList/>
     </>
   );
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,11 +1,12 @@
 import './Hero.scss';
 
 function Hero() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div className="hero">
+            <h1 className="hero-title">Find AI solutions that are right for your business</h1>
+            <h3 className="hero-subhead">No matter what challenges your business is facing, we are here to help</h3>
+        </div>
+    )
 }
 
 export default Hero;
