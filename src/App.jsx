ae255b9682dd0c804d38e7d37196d36e1f63d955
Julia Babicheva <julia.bbchv@gmail.com>
2025-02-13 18:08:37 -0800

    connect generate response functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.scss";
+import ResponseWindow from "./ResponseWindow/ResponseWindow";
 
 function App() {
   return (
     <>
       <h1>This is our Industry Project</h1>
+      <ResponseWindow />
     </>
   );
 }
