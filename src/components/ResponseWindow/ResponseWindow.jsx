10d6fa2e944833459539069c36f1a547d90b5640
Julia Babicheva <julia.bbchv@gmail.com>
2025-02-14 09:20:25 -0800

    pass state to challanges input, finilize generate response functionality

--- src/components/ResponseWindow/ResponseWindow.jsx
+++ src/components/ResponseWindow/ResponseWindow.jsx
@@ -2,35 +2,27 @@ import { useState } from "react";
 import axios from "axios";
 import "./ResponseWindow.scss";
 
-export default function ResponseWindow() {
-  const [aiResponse, setAiResponse] = useState("");
-  const [loading, setLoading] = useState(false);
-  const [formData, setFormData] = useState({
-    products: "MS Teams",
-    industry: "automotive",
-    painPoints: "data management",
-  });
+export default function ResponseWindow({ response }) {
+  // const [aiResponse, setAiResponse] = useState("");
+  // const [loading, setLoading] = useState(false);
 
-  console.log(setFormData);
+  // const generateResponse = async () => {
+  //   if (loading) return;
+  //   setLoading(true);
 
-  const generateResponse = async () => {
-    if (loading) return;
-    setLoading(true);
-
-    try {
-      const response = await axios.post(
-        "http://localhost:8080/api/openai",
-        formData
-      );
-      console.log(response.data);
-      setAiResponse(response.data || "No response received.");
-    } catch (err) {
-      console.error("Error generating response:", err);
-    } finally {
-      setLoading(false);
-    }
-  };
-
+  //   try {
+  //     const response = await axios.post(
+  //       "http://localhost:8080/api/openai",
+  //       formData
+  //     );
+  //     console.log(response.data);
+  //     setAiResponse(response.data || "No response received.");
+  //   } catch (err) {
+  //     console.error("Error generating response:", err);
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // };
   return (
     <section className="response__container">
       <div className="response">
@@ -38,12 +30,9 @@ export default function ResponseWindow() {
           className="response__box"
           readOnly
           placeholder="How can we help you today?"
-          value={aiResponse}
+          value={response}
         />
       </div>
-      <button onClick={generateResponse} disabled={loading}>
-        {loading ? "Generating..." : "Generate New Response"}
-      </button>
     </section>
   );
 }
