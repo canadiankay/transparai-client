d731feeab11e6bea77c99ceb0ad30323763e58bd
Nadia Kasim <nadiakasim@hotmail.com>
2025-02-13 22:59:22 -0500

    created form component emoty and linked to app jsx to see my input component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import "./App.scss";
 import ResourcesList from "./components/ResourcesList/ResourcesList.jsx";
 import Nav from "./components/Nav/Nav.jsx";
+import Form from "./components/Form/Form.jsx";
 
 function App() {
   return (
     <>
       <Nav />
 
+      <Form />
 
-
-      
-      <ResourcesList/>
+      <ResourcesList />
     </>
   );
 }
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,30 +1,36 @@
-import './Nav.scss';
-import cartIcon from "../../images/shopping-cart.svg"
-import microsoft from "../../images/microsoft-header.png";
-import searchIcon from "../../images/search-24px.svg";
+import "./Nav.scss";
+import cartIcon from "../../assets/images/shopping-cart.svg";
+import microsoft from "../../assets/images/microsoft-logo.png";
+import searchIcon from "../../assets/images/search-icon.svg";
 
 function Nav() {
-    return (
-      <div className="nav">
-        <section className="header-nav">
-          <div className="nav-left">
-            <img src={microsoft} alt="microsoft logo" className="nav-logo" />
-            <hr/>
-            <p className="nav-text">Small Business</p>
-            <p className="nav-text">Surface</p>
-            <p className="nav-text">Software</p>
-          </div>
-          <div className="nav-right">
-            <p className="nav-text">All Microsoft</p>
-            <p className="nav-text">Search <img src={searchIcon} alt="search icon" /></p>
-            <p className="nav-text">Cart <img src={cartIcon} alt="cart icon" /></p>
-          </div>
-        </section>
-        <div className="nav-subhead">
-            <h2 className="nav-subhead__text">Find great deals for your business</h2>
+  return (
+    <div className="nav">
+      <section className="header-nav">
+        <div className="nav-left">
+          <img src={microsoft} alt="microsoft logo" className="nav-logo" />
+          <hr />
+          <p className="nav-text">Small Business</p>
+          <p className="nav-text">Surface</p>
+          <p className="nav-text">Software</p>
         </div>
+        <div className="nav-right">
+          <p className="nav-text">All Microsoft</p>
+          <p className="nav-text">
+            Search <img src={searchIcon} alt="search icon" />
+          </p>
+          <p className="nav-text">
+            Cart <img src={cartIcon} alt="cart icon" />
+          </p>
+        </div>
+      </section>
+      <div className="nav-subhead">
+        <h2 className="nav-subhead__text">
+          Find great deals for your business
+        </h2>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
 export default Nav;
